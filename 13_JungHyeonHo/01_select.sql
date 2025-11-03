bdd8e4cdd2e1f806802fa6b3af1d90cab13f8101
gusgh075 <gusgh075@gmail.com>
2025-11-03 14:37:18 +0900

    추가내용

--- 13_JungHyeonHo/01_select.sql
+++ 13_JungHyeonHo/01_select.sql
@@ -23,4 +23,22 @@ SELECT *
 FROM tbl_menu;
 
 -- 단독 select 활용
+-- == 선택할 테이블이 없다
+-- == 어떠한 테이블도 조회하려는 데이터를 가지고 있지 않다
 select 7+3;
+select 7%3; -- 나머지(Modulo,mod);=
+select 7/3;
+
+select 7+3,7-3; -- 여러 컬럼 조회도 가능
+
+-- 내장 함수 이용
+select now() as 현재시간; -- 현재 시간
+# select concat('/홍',' ','길동') as 'full name';-- 내 문자열을 합쳐서 결과 출력
+select concat('/홍',' ','길동') as 'full name';
+                                             -- 내 문자열을 합쳐서 결과 출력
+
+select tbl_order,
+tbl_order_menu,
+tbl_payment,
+tbl_payment_order
+from tbl_menu''
