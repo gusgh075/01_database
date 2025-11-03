8251738dde8806196b6bee6752f59445975771fa
jabbit1999 <jabbit1999@gmail.com>
2025-11-03 14:20:12 +0900

    swcamp 계정 생성, DB_Script, 01_SELECT 진행중

---
+++ 07_SonChangWoo/01_select.sql
@@ -0,0 +1,24 @@
+-- 01_SELECT : 특정 테이블의 원하는 데이터(행)을 조회하는 구문
+
+-- tbl_menu 테이블의 모든 메뉴 이름(menu_name) 조회
+/*범위 주석 (단축키 ctrl + shift _ /)*/
+
+
+/*2*/
+SELECT
+   menu_name
+/*1*/
+FROM
+	tbl_menu;
+
+-- 모든 메뉴의 menu_code, menu_name, menu_price 조회
+SELECT
+   menu_code, menu_name, menu_price
+FROM
+	tbl_menu;
+
+-- tbl_menu 테이블 모든 열 조회, *(asterisk) == 모든
+SELECT
+   *
+FROM
+	tbl_menu;
